ab1e156da2bf6b7862b787955b1274704b122143
Fabio Altavini <f.altavini@sv-gruppe.de>
2025-02-24 16:11:22 +0100

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {MatDialog} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,50 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'dummy17';
+  title = 'Angular v17';
+  loginSuccess = false;
+  loginFailed = false;
+  username: string = "";
+  password: string = "";
+  currentSsoKey: string = "";
+  ssoKeyFromA4201: string | null = "";
+
+  constructor() {
+  }
+
+  ngOnInit() {
+
+    this.ssoKeyFromA4201 = this.getSSOKey();
+    if (this.ssoKeyFromA4201) {
+      this.loginSuccess = true;
+    } else {
+      this.loginFailed = true;
+    }
+    console.log(this.ssoKeyFromA4201);
+  }
+
+  login() {
+    console.log("Username", this.username);
+    console.log("Passwort", this.password);
+    this.generateSSOKey();
+    console.log('SSO Key', this.currentSsoKey);
+  }
+
+  performLogin() {
+    if (this.getSSOKey()) {
+      return 'success';
+    } else {
+      return 'failure';
+    }
+  }
+
+  generateSSOKey() {
+    //TODO: set SSO Key (Math.random ersetzen)
+    localStorage.setItem('ssoKey', Math.random().toString(36).substr(2, 9));
+    this.currentSsoKey = Math.random().toString(36).substr(2, 9);
+  }
+
+  getSSOKey() {
+    return localStorage.getItem('ssoKey');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {MatCard} from "@angular/material/card";
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {MatButton} from "@angular/material/button";
+import {MatIcon} from "@angular/material/icon";
+import {MatTooltip} from "@angular/material/tooltip";
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,9 +18,19 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    MatCard,
+    MatFormField,
+    MatLabel,
+    MatInput,
+    MatButton,
+    MatIcon,
+    MatTooltip
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
